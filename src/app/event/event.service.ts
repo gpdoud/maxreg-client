cd6074059ee4101865f4c8b98ab5b22f11be7dd6
Kellan Arick <kellan5015@gmail.com>
2019-03-29 15:56:53 -0400

    event service

--- src/app/event/event.service.ts
+++ src/app/event/event.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Event } from './event.class';
 
-const url = "http://localhost:52132/api/"
+const url = 'http://localhost:56290/api/';
 
 @Injectable
 ({
@@ -8,6 +11,30 @@ const url = "http://localhost:52132/api/"
 })
 export class EventService 
 {
+  list(): Observable<Event[]>
+  {
+    return this.http.get(`${url}/events`) as Observable<Event[]>;
+    
+  }
+  get(id:string): Observable<Event>
+  {
+    return this.http.get(`${url}/events/${id}`) as Observable<Event>;
+    
+  }
+  create(event:Event): Observable<any>
+  {
+    return this.http.post(`${url}/events`, event) as Observable<any>;
+  }
 
-  constructor() { }
+  change(event:Event): Observable<any>
+  {
+    return this.http.put(`${url}/events/${event.id}`, event) as Observable<any>;
+  }
+
+  remove(event:Event): Observable<any>
+  {
+    return this.http.delete(`${url}/events/${event.id}`) as Observable<any>;
+  }
+
+  constructor(private http: HttpClient) { }
 }
