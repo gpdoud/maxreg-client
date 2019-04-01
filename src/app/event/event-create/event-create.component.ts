f86eae1b79f0ee3d825d005673a90b7e2a3ddeeb
Chris Caudill <christopherscaudill@gmail.com>
2019-04-01 18:28:37 -0400

    added event

--- src/app/event/event-create/event-create.component.ts
+++ src/app/event/event-create/event-create.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { EventService } from '../event.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import{Event} from '../event.class';
 
 @Component({
   selector: 'app-event-create',
@@ -6,10 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./event-create.component.css']
 })
 export class EventCreateComponent implements OnInit {
+ 
+  event: Event = new Event;
+  // toD: Date = new Date();
+  // toDs = this.toD.toISOString();
 
-  constructor() { }
+  constructor(
+    private esrv: EventService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  save(){
+    // if(this.event.date == "")
+    //   {
+    //     this.event.date = this.toDs;
+    //   }
+    this.esrv.create(this.event)
+    .subscribe(
+      respond => { //success
+        console.log(respond);
+        this.router.navigateByUrl('/event/list');
+      },
+      err =>{ //error
+        console.error(err);
+      }
+    );
+
   }
 
 }
