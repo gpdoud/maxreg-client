5086d03f7dd52fe5d43d02d7b7ec4adec8f763b1
ben Petersen <ben83pete@gmail.com>
2019-04-02 15:56:07 -0400

    home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Event } from '../event/event.class';
+import { EventService } from '../event/event.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  todayDate = (new Date()).toISOString();
+  events: Event[];
+  todayEvents: Event[];
+
+  constructor(private esrvc: EventService, ) {
+    
+   }
 
   ngOnInit() {
+    this.esrvc.list()
+      .subscribe(resp =>{
+        console.log(resp);
+        this.events = resp;
+
+        console.log(this.todayDate);
+      })
   }
 
 }
